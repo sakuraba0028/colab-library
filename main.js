e4b1c08bb492fe9c0aa81ef814bfd6b45d479f06
sakuraba0028 <sakuraba0028@gmail.com>
2026-09-02 20:53:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-const consoleLog = log => console.log(log);
+const outputConsoleLog = log => {
+    let consoleLog = document.querySelector('.console');
+    const hasConsole = consoleLog ? true : false;
+
+    if (hasConsole) {
+        consoleLog = document.querySelector('p')
+        consoleLog.classList.add('console');
+        document.querySelector(':is(.container, .main-container)').appendChild(consoleLog);
+    }
+
+    consoleLog.innerText = log;
+};
 
 for (const input of document.querySelectorAll('input')) {
     const { name } = input;
