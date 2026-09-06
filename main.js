f36e0e256601025b9bc131abdf10fdf4726002c0
sakuraba0028 <sakuraba0028@gmail.com>
2026-09-06 11:43:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-const colabConsole = log => {
-    let consoleLog = document.querySelector('.console');
-    const hasConsoleLog = consoleLog ? true : false;
-
-    if (!hasConsoleLog) {
-        consoleLog = document.createElement('p');
-        consoleLog.classList.add('console');
-        document.querySelector(':is(.container, .main-container)').appendChild(consoleLog);
-    }
-
-    consoleLog.innerText += `\n${log}`;
-};
-
-for (const input of document.querySelectorAll('input')) {
+for (const input of document.querySelectorAll('input') {
     const { name } = input;
-    const replacedName = name.replace(/-/g, () => ' ');
-    const hasPlaceholder = input.value ? true : false;
-
-    if (!hasPlaceholder) input.placeholder = `Please enter the ${replacedName}`;
+    const replacedName = name.replace(/-/g, ' ');
+    const placeholder = `Please enter the ${replacedName}`;
+    input.placeholder = placeholder;
 }
