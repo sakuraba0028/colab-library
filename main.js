4558790561e656df282a5460ed66183a5c7710e3
sakuraba0028 <sakuraba0028@gmail.com>
2026-09-06 11:47:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-for (const input of document.querySelectorAll('input') {
-    const { name } = input;
-    const replacedName = name.replace(/-/g, ' ');
-    const placeholder = `Please enter the ${replacedName}`;
-    input.placeholder = placeholder;
-}
+const setInputPlaceholder = (
+    for (const input of document.querySelectorAll('input') {
+        const { name } = input;
+        const replacedName = name.replace(/-/g, ' ');
+        const placeholder = `Please enter the ${replacedName}`;
+        input.placeholder = placeholder;
+    }
+};
