720a046f7103996ce223f427784582ada30b5cf9
sakuraba0028 <sakuraba0028@gmail.com>
2026-09-06 14:27:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,62 @@
-const resetCopyButton = copyButton => copyButton.dataset.isCopied = false;
-
-for (const textInput of document.querySelectorAll('input')) {
-    if (textInput.type !== 'text') continue;
-
-    const { name } = textInput;
-    const humanizedName = humanize(name);
-    textInput.type = 'text'; // textを代入することでcssやquerySelectorで取得できる
-    textInput.placeholder = `Please enter the ${humanizedName}`;
-}
-
-for (const option of document.querySelectorAll('option')) {
-    const { value } = option;
-    const humanizedValue = humanize(value);
-    option.textContent = humanizedValue;
-}
-
-for (const submitButton of document.querySelectorAll('button')) {
-    const parentIsForm = submitButton.parentElement.tagName === 'FORM';
-    if (!parentIsForm) continue;
-
-    submitButton.innerText = 'Submit';
-}
-
-for (const copyButton of document.querySelectorAll('.copy-button')) {
-    const observer = new MutationObserver(mutationsList => {
-        for (const mutation of mutationsList) {
-            if (mutation.type === 'attributes') {
-                const attrName = mutation.attributeName;
-                const newValue = copyButton.getAttribute(attrName);
-
-                if (newValue === 'true') {
-                    copyButton.textContent = 'Copy completed!';
-                    navigator.clipboard.writeText(copyButton.value);
-                } else {
-                    copyButton.textContent = 'Copy';
-                }
-            }
+const initElement = ( 
+    const initForm = () => {
+        for (const form of document.querySelectorAll('form')) {
+            form.addEventListener('click', e => e.preventDefault());
         }
-    });
-    observer.observe(copyButton, {
-        attributes: true,
-        attributeFilter: ['data-is-copied']
-    });
 
-    copyButton.dataset.isCopied = false;
-    copyButton.addEventListener('click', e => e.target.dataset.isCopied = true);
-}
+        for (const textInput of document.querySelectorAll('input')) {
+            if (textInput.type !== 'text') continue;
+        
+            const { name } = textInput;
+            const humanizedName = humanize(name);
+            textInput.type = 'text'; // textを代入することでcssやquerySelectorで取得できる
+            textInput.placeholder = `Please enter the ${humanizedName}`;
+        }
+
+        for (const submitButton of document.querySelectorAll('button')) {
+            const parentIsForm = submitButton.parentElement.tagName === 'FORM';
+            if (!parentIsForm) continue;
+        
+            submitButton.innerText = 'Submit';
+        }
+    };
+
+    const initSelect = () => {
+        for (const option of document.querySelectorAll('option')) {
+            const { value } = option;
+            const humanizedValue = humanize(value);
+            option.textContent = humanizedValue;
+        }
+    };
+
+    const initCopyButton = () => {
+        for (const copyButton of document.querySelectorAll('.copy-button')) {
+            const observer = new MutationObserver(mutationsList => {
+                for (const mutation of mutationsList) {
+                    if (mutation.type === 'attributes') {
+                        const attrName = mutation.attributeName;
+                        const newValue = copyButton.getAttribute(attrName);
+        
+                        if (newValue === 'true') {
+                            copyButton.textContent = 'Copy completed!';
+                            navigator.clipboard.writeText(copyButton.value);
+                        } else {
+                            copyButton.textContent = 'Copy';
+                        }
+                    }
+                }
+            });
+            observer.observe(copyButton, {
+                attributes: true,
+                attributeFilter: ['data-is-copied']
+            });
+        
+            copyButton.dataset.isCopied = false;
+            copyButton.addEventListener('click', e => e.target.dataset.isCopied = true);
+        }
+    };
+
+    initForm();
+    initSelect();
+    initCopyButton();
+};
